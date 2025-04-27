cab6d8e87026fc0c5fa4308967b021585cb82c02
Bkhamilton <bkhamilton310@gmail.com>
2025-04-26 23:47:01 -0400

    added database caching, user picked favorites for APOD

--- django-space-api/space_explorer/models.py
+++ django-space-api/space_explorer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class APOD(models.Model):
+    title = models.CharField(max_length=200)
+    explanation = models.TextField()
+    url = models.URLField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    apod = models.ForeignKey(APOD, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- django-space-api/space_explorer/views.py
+++ django-space-api/space_explorer/views.py
@@ -1,11 +1,20 @@
 import requests
 from django.http import JsonResponse
 from django.conf import settings
+from .models import APOD, Favorite
+from django.contrib.auth.decorators import login_required
 
 def apod(request):
-    url = f'https://api.nasa.gov/planetary/apod?api_key={settings.NASA_API_KEY}'
-    response = requests.get(url)
-    return JsonResponse(response.json())
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={settings.NASA_API_KEY}').json()
+    
+    # Save to database
+    APOD.objects.create(
+        title=response['title'],
+        explanation=response['explanation'],
+        url=response['url']
+    )
+    
+    return JsonResponse(response)
 
 def launches(request):
     url = 'https://llapi.launchlibrary.net/1.4/launch'
@@ -21,3 +30,9 @@ def asteroids(request):
     url = f'https://api.nasa.gov/neo/rest/v1/feed?api_key={settings.NASA_API_KEY}'
     response = requests.get(url)
     return JsonResponse(response.json())
+
+@login_required
+def favorite_apod(request, apod_id):
+    apod = APOD.objects.get(id=apod_id)
+    Favorite.objects.create(user=request.user, apod=apod)
+    return JsonResponse({"status": "success"})
